90bd9136385f0dd7ebe1686cc2148aa6723e2a1d
TruLeroyalty <98649663+TruLeroyalty@users.noreply.github.com>
2023-05-10 03:44:16 -0500

    Created variables for the forms on HTML page.

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-console.log('hello world');
+fetch('http://localhost:3000/Philosophers')
+.then(res => res.json())
+.then(data => {
 
-fetch('http://localhost:3000/Philosophers').then((res) => res.json()).then(json => console.log(json))
+    // Creating variables for the forms on the html page. 
+
+    const form = document.querySelector('form');
+    const culture = document.getElementById('cultureSelect');
+    const PhilosopherInput = document.getElementById('pname');
+    const submitBtn = document.getElementById('submission');
+    const PhilosopherInfo = document.getElementById('philosopherInfo');
+
+})

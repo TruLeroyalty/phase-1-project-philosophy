6ad8af7e9b435eced5482babcde66b71d5be7f93
TruLeroyalty <98649663+TruLeroyalty@users.noreply.github.com>
2023-05-11 15:31:16 -0500

    shows selected culture's philosophers.

--- main.js
+++ main.js
@@ -6,17 +6,29 @@ fetch('http://localhost:3000/Philosophers')
 
     const form = document.querySelector('form');
     const culture = document.getElementById('cultureSelect');
-    const PhilosopherInput = document.getElementById('pname');
+    const philosopherInput = document.getElementById('pname');
     const submitBtn = document.getElementById('submission');
     const PhilosopherInfo = document.getElementById('philosopherInfo');
 
+    //Disables the Submit button and Philosopher drop-down unless you pick a culture first. 
+
     culture.addEventListener('change', function(e) {
         if(culture.value === 'Ancient Greece' || culture.value === 'Ancient Egypt' || culture.value === 'Ancient China' || culture.value === 'Ancient India') {
-            PhilosopherInput.disabled = false;
+            philosopherInput.disabled = false;
             submitBtn.disabled = false;
     } else {
-        PhilosopherInput.disabled = true;
+        philosopherInput.disabled = true;
         submitBtn.disabled = true;
     }
 })
-})
+
+            // Grabs the data and filters it, if it matches a philosopher, they will show up.
+
+function filterPeopleByCulture(selectedCulture, philosopherData) {
+    const filteredPeople = philosopherData.filter(function(philosopher) {
+        return philosopher.culture === selectedCulture;
+    });
+
+}
+});
+
